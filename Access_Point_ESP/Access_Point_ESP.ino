3ee9c66d919e0476db6a86c9235e01fa66f06714
khaitheguy <58598082+khaitheguy@users.noreply.github.com>
2024-11-11 15:24:33 +0800

    Receive sensor readings in AP via MQTT

--- Access_Point_ESP/Access_Point_ESP.ino
+++ Access_Point_ESP/Access_Point_ESP.ino
@@ -12,7 +12,28 @@ IPAddress subnet(255, 255, 255, 0);
 
 // MQTT details
 const char* mqttServer = "mqtt://192.168.4.3:1883";
+
+char *alarmTopic = "img";
+char *clearAlarmTopic = "deactivate";
+char *tempTopic = "tem";       // Topic for temperature
+char *humidTopic = "hum"; // Topic for humidity
+char *gasTopic = "gas";         // Topic for gas concentration
+char *lightTopic = "lux";     // Topic for light level
+
 ESP32MQTTClient mqttClient; // all params are set later
+
+// Set global flags
+bool alarmActivated = false;
+bool tempReceived = false;
+bool humidReceived = false;
+bool gasReceived = false;
+bool lightReceived = false;
+
+// Containers to hold values from readings
+String temp;
+String humid;
+String gas;
+String light;
 
 void setup() {
   Serial.begin(115200);
@@ -34,14 +55,61 @@ void setup() {
 }
 
 void loop() {
-  // Nothing to do here
+  if (alarmActivated) {
+    
+      // Do not clear the alarm until a signal is received
+      while (alarmActivated)
+      {
+        delay(10);
+      }
+    
+  }
 }
 
 void onMqttConnect(esp_mqtt_client_handle_t client)
 {
     if (mqttClient.isMyTurn(client)) // can be omitted if only one client
     {
-        // For MQTT subscribe
+        mqttClient.subscribe(alarmTopic, [](const String &payload)
+            {
+              if (String(payload.c_str()) == "1") {
+                alarmActivated = true;
+              }
+            });
+
+        mqttClient.subscribe(clearAlarmTopic, [](const String &payload)
+            {
+              if (String(payload.c_str()) == "1") {
+                alarmActivated = false;
+              }
+            });
+
+        mqttClient.subscribe(tempTopic, [](const String &payload)
+            {
+              tempReceived = true;
+              temp = String(payload.c_str());
+            });
+
+        // Subscribe to humidity topic
+        mqttClient.subscribe(humidTopic, [](const String &payload)
+          {
+              humidReceived = true;
+              humid = String(payload.c_str()); // Assign payload to humid
+          });
+
+        // Subscribe to gas concentration topic
+        mqttClient.subscribe(gasTopic, [](const String &payload)
+          {
+              gasReceived = true;
+              gas = String(payload.c_str()); // Assign payload to gas
+          });
+
+        // Subscribe to light level topic
+        mqttClient.subscribe(lightTopic, [](const String &payload)
+          {
+              lightReceived = true;
+              light = String(payload.c_str()); // Assign payload to light
+          });
     }
 }
 
